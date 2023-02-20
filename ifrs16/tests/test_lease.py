76296739d50750b6505bb5554f4b7210ac9566a6
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-02-20 15:42:39 +0000

    Add docstrings to lease tests

--- ifrs16/tests/test_lease.py
+++ ifrs16/tests/test_lease.py
@@ -8,11 +8,19 @@ from ifrs16.tests.factories import LeaseFactory
 
 class TestSetDates:
     def test_both_strings(self):
+        '''
+        Lease date arguments passed in as correctly formatted strings are both
+        set as datetime attributes.
+        '''
         lease = LeaseFactory(start_date='01/01/2022', end_date='01/01/2023')
         assert isinstance(lease.start_date, datetime)
         assert isinstance(lease.end_date, datetime)
 
     def test_both_datetimes(self):
+        '''
+        Lease date arguments both passed in as datetimes are both set as
+        datetime attributes.
+        '''
         lease = LeaseFactory(
             start_date=datetime(2022, 1, 1), end_date=datetime(2023, 1, 1)
         )
@@ -20,6 +28,10 @@ class TestSetDates:
         assert isinstance(lease.end_date, datetime)
 
     def test_string_and_datetime(self):
+        '''
+        Lease date arguments where start date is a string but end date is a
+        datetime are both set as datetime attributes.
+        '''
         lease = LeaseFactory(
             start_date='01/01/2022', end_date=datetime(2023, 1, 1)
         )
@@ -27,6 +39,10 @@ class TestSetDates:
         assert isinstance(lease.end_date, datetime)
 
     def test_datetime_and_string(self):
+        '''
+        Lease date arguments where start date is a datetime but end date is a
+        string are both set as datetime attributes.
+        '''
         lease = LeaseFactory(
             start_date=datetime(2022, 1, 1), end_date='01/01/2023'
         )
@@ -34,22 +50,26 @@ class TestSetDates:
         assert isinstance(lease.end_date, datetime)
 
     def test_invalid_types(self):
+        '''Lease date argument of wrong type raises `TypeError`.'''
         with pytest.raises(TypeError):
             # incorrect integer `end_date` is neither string or datetime
             LeaseFactory(start_date='01/01/2022', end_date=42)
 
     def test_invalid_date_format(self):
+        '''Lease date string with incorrect format raises `ValueError`.'''
         with pytest.raises(ValueError):
             # incorrect format for `end_date`
             LeaseFactory(start_date='01/01/2022', end_date='2023-01-01')
 
     def test_start_after_end_date(self):
+        '''Start date after end date raises `StartDateAfterEndDateError`.'''
         with pytest.raises(StartDateAfterEndDateError):
             LeaseFactory(start_date='02/01/2023', end_date='01/01/2023')
 
 
 class TestTerm:
     def test_get_term(self):
+        '''Lease term is duration from start date to end date.'''
         lease = LeaseFactory(start_date='01/01/2022', end_date='01/01/2023')
         expected_term = lease.end_date - lease.start_date
         assert lease.term == expected_term
@@ -57,31 +77,44 @@ class TestTerm:
 
 class TestShortTerm:
     def test_exact_short_term(self):
+        '''Lease is short term when dates are exactly 12 months apart.'''
         lease = LeaseFactory(start_date='01/01/2022', end_date='01/01/2023')
         assert lease.is_short_term()
 
     def test_comfortably_short_term(self):
+        '''
+        Lease is short term when dates are clearly less than 12 months apart.
+        '''
         lease = LeaseFactory(start_date='01/01/2022', end_date='01/05/2022')
         assert lease.is_short_term()
 
     def test_nearly_short_term(self):
+        '''
+        Lease is not short term when dates are slightly over 12 months apart.
+        '''
         lease = LeaseFactory(start_date='31/12/2021', end_date='01/01/2023')
         assert not lease.is_short_term()
 
     def test_comfortably_not_short_term(self):
+        '''
+        Lease is not short term when dates are clearly over 12 months apart.
+        '''
         lease = LeaseFactory(start_date='01/01/2022', end_date='01/01/2024')
         assert not lease.is_short_term()
 
 
 class TestTenure:
     def test_leasehold(self):
+        '''Valid leasehold tenure string is set as tenure attribute.'''
         lease = LeaseFactory(tenure='leasehold')
         assert lease.tenure == 'leasehold'
 
     def test_freehold(self):
+        '''Valid freehold tenure string is set as tenure attribute.'''
         lease = LeaseFactory(tenure='freehold')
         assert lease.tenure == 'freehold'
 
     def test_invalid_tenure(self):
+        '''Invalid tenure string raises `InvalidTenureError`.'''
         with pytest.raises(InvalidTenureError):
             LeaseFactory(tenure='invalid_tenure')
