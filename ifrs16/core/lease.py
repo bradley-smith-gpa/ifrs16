0020f288e290e152e9d21b3d38a55f9da2e1a313
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-02-20 12:34:33 +0000

    Add docstrings to `Lease`

--- ifrs16/core/lease.py
+++ ifrs16/core/lease.py
@@ -6,14 +6,17 @@ from ifrs16.core.exceptions import (
 
 
 class Lease:
+    '''Basic lease object.'''
     valid_tenure_choices = ('leasehold', 'freehold')
 
     def __init__(self, start_date, end_date, tenure):
+        '''Initialise lease object along with validation of args.'''
         self._set_dates(start_date, end_date)
         self.term = self.end_date - self.start_date
         self._set_tenure(tenure)
 
     def _set_dates(self, start_date, end_date):
+        '''Set date attributes as datetimes if they are valid.'''
         raw_dates = {'start_date': start_date, 'end_date': end_date}
         dates = {}
         for attr, raw_date in raw_dates.items():
@@ -31,12 +34,14 @@ class Lease:
             setattr(self, attr, date)
 
     def _set_tenure(self, tenure):
+        '''Set tenure attributes if it is valid.'''
         if tenure in self.valid_tenure_choices:
             self.tenure = tenure
         else:
             raise InvalidTenureError(tenure, self.valid_tenure_choices)
 
     def is_short_term(self):
+        '''Return `True` if lease duration is 12 months or less.'''
         threshold_date = self.end_date - relativedelta(months=12)
         is_short_term = (self.start_date >= threshold_date)
         return is_short_term
