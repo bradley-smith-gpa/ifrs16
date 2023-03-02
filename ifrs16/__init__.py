1feca3a171e144d62eba5a51c538e257766a0f3c
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-03-02 09:30:51 +0000

    Add `Organisation` to `__name__`

--- ifrs16/__init__.py
+++ ifrs16/__init__.py
@@ -1,5 +1,6 @@
 __doc__ = '''Generate simple lease calculations with respect to IFRS 16'''
 __version__ = '0.0.1'
-__all__ = ['Lease']
+__all__ = ['Lease', 'Organisation']
 
 from ifrs16.core.lease import Lease
+from ifrs16.core.organisation import Organisation
