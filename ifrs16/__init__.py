2d03f37be45401538aad805a7a3ba89f1d2b6f93
bradley-smith-gpa <bradley.smith@gpa.gov.uk>
2023-03-07 09:48:45 +0000

    Use semantic version to indicate development phase

--- ifrs16/__init__.py
+++ ifrs16/__init__.py
@@ -1,5 +1,5 @@
 __doc__ = '''Generate simple lease calculations with respect to IFRS 16'''
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __all__ = ['Lease', 'Organisation']
 
 from ifrs16.core.lease import Lease
